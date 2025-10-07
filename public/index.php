efe281e24e6a66257f78c071155ae41da421f4a0
Heitor Zelbato <heitorzelbato@gmail.com>
2025-10-07 20:40:00 -0300

    First config

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste Tailwind</title>
+    <link rel="stylesheet" href="assets/css/output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="flex justify-center items-center min-h-screen bg-gray-100">
+
+    <div class="bg-red-500 text-white p-6 rounded-xl shadow-xl text-2xl font-bold">
+        Tailwind voltou a funcionar! 🚀
+    </div>
+
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,php,js,css}",
+    "./src/**/*.{html,php,js,scss}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
