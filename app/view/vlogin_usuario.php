3a546ca6ed83633e68ff63700ac25afb38f14f84
joaopedrogianini <joaopedrogianinifranzin@gmail.com>
2025-10-21 22:06:11 -0300

    Fiz o css do Login e do cadastro, arrumei algumas cores da variables

--- app/view/vlogin_usuario.php
+++ app/view/vlogin_usuario.php
@@ -9,7 +9,7 @@ require_once '../DADOS/config.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
- <link rel="stylesheet" href="../../public/assets/scss/style.css"><!--IMPORTANTE -->
+ <link rel="stylesheet" href="../../public/assets/css/login.css"><!--IMPORTANTE -->
 </head>
 
 <body>
